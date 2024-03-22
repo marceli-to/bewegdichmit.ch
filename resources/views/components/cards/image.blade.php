d63e7d05daab193733856e8d35b714e8aea0dea0
Marcel Stadelmann <m@marceli.to>
2024-03-22 19:11:23 +0100

    wip

--- resources/views/components/cards/image.blade.php
+++ resources/views/components/cards/image.blade.php
@@ -1,5 +1,5 @@
 @props(['img', 'alt' => 'Image'])
-<div class="col-span-2 sm:col-span-1 aspect-square bg-gray-200">
+<div class="col-span-2 sm:col-span-1 aspect-square bg-sky">
   <img 
     src="/media/img/runners/{{ $img }}" 
     alt="{{ $alt }}" 
