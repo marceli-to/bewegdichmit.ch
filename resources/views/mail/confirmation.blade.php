9b518cb9d268250684239d8e602441597b440682
Marcel Stadelmann <m@marceli.to>
2024-03-23 14:06:23 +0100

    wip

--- resources/views/mail/confirmation.blade.php
+++ resources/views/mail/confirmation.blade.php
@@ -11,8 +11,7 @@
       Asylstrasse 24<br>
       8610 Uster<br><br>
       Zeit Check-In: ab 9.30 Uhr<br>
-      Bitte hole deine Startnummer im Wagi-Dorfbüro spätestens 30 Minuten vor deiner <strong>gewählten Startzeit ab</strong>.<br>
-      Du startest in der Läufergruppe um: {{ $data->start_time }} Uhr.<br><br>
+      Bitte hole deine Startnummer im Wagi-Dorfbüro spätestens <strong>30 Minuten vor deiner gewählten Startzeit ab. Du startest in der Läufergruppe um: {{ $data->start_time }} Uhr.</strong><br><br>
       Start 1. Läufergruppe: 10.30 Uhr<br>
       Start 2. Läufergruppe: 11.15 Uhr<br>
       Start 3. Läufergruppe: 12.00 Uhr<br><br>
@@ -33,7 +32,7 @@
   @if ($data->category == 'group')
   <div class="text-base pb-base">
     Liebe Teilnehmende der Familie/Gruppe <strong>{{ $data->group_name }}</strong><br><br>
-    Vielen Dank für eure Teilnahme als Familie/Gruppe von <strong>{{ $data->number_of_members }}</strong> Personen. Wir freuen uns sehr, euch bald bei uns im Wagerenhof zu begrüssen. Gemeinsam umrunden wir das Wagi-Quartier (ca. 500m) und geniessen einen sportlichen Tag mit allerlei Attraktionen, einer vielseitigen Foodmeile und Live Musik.<br><br>
+    Vielen Dank für eure Teilnahme als <strong>Familie/Gruppe von {{ $data->number_of_members }}</strong> Personen. Wir freuen uns sehr, euch bald bei uns im Wagerenhof zu begrüssen. Gemeinsam umrunden wir das Wagi-Quartier (ca. 500m) und geniessen einen sportlichen Tag mit allerlei Attraktionen, einer vielseitigen Foodmeile und Live Musik.<br><br>
     Wir bitten euch, den gewählten Betrag von <strong>CHF {{ number_format($data->entry_fee, 2, '.', '.') }}</strong> bis zum 25. Mai 2024 mit angefügtem Einzahlungsschein zu begleichen.<br><br>
     Mit eurer Teilnahme trägt ihr zur Gesundheit unserer Bewohnerinnen und Bewohner bei und unterstützt im Rahmen unseres Spendenprojektes Gesundheit und Fitness unter anderem die Realisierung eines barrierefreien und bedarfsgerechten Fitnessraums im Wagerenhof. Herzlichen Dank!<br><br>
     <strong>Programm</strong><br>
@@ -42,8 +41,7 @@
     Asylstrasse 24<br>
     8610 Uster<br><br>
     Zeit Check-In: ab 9.30 Uhr<br>
-    Bitte hole deine Startnummer im Wagi-Dorfbüro spätestens 30 Minuten vor deiner <strong>gewählten Startzeit ab</strong>.<br>
-    Du startest in der Läufergruppe um: {{ $data->start_time }} Uhr.<br><br>
+    Bitte hole deine Startnummer im Wagi-Dorfbüro spätestens <strong>30 Minuten vor deiner gewählten Startzeit ab. Du startest in der Läufergruppe um: {{ $data->start_time }} Uhr.</strong><br><br>
     Start 1. Läufergruppe: 10.30 Uhr<br>
     Start 2. Läufergruppe: 11.15 Uhr<br>
     Start 3. Läufergruppe: 12.00 Uhr<br><br>
