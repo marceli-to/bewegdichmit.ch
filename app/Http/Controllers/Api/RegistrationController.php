af58ee6fa4c7337cb2816423e1c602eecce1f633
Marcel Stadelmann <m@marceli.to>
2024-03-25 12:47:00 +0100

    wip

--- app/Http/Controllers/Api/RegistrationController.php
+++ app/Http/Controllers/Api/RegistrationController.php
@@ -54,6 +54,9 @@ class RegistrationController extends Controller
     // if a start time has 100 registrations, it is fully booked and should not be available for selection
     $max_slots = 100;
 
+    $default_slots = ['10.30' => $max_slots, '11.15' => $max_slots, '12.00' => $max_slots];
+    return response()->json(collect($default_slots->keys()));
+
     $slots = Registration::select('start_time', \DB::raw('count(*) as total'))
       ->groupBy('start_time')
       ->get()
