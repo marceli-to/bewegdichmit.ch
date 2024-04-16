1e0f4c0e424edfd95a62f607360a0557ae369194
Marcel Stadelmann <m@marceli.to>
2024-04-16 15:31:30 +0200

    wip

--- resources/views/components/partials/together.blade.php
+++ resources/views/components/partials/together.blade.php
@@ -5,55 +5,55 @@
     </x-headings.h1>
     <div class="grid grid-cols-4 gap-16 lg:gap-20">
       <x-cards.quote quote="Ich bin mit meinem Ferrari schnell unterwegs." author="Francesco" class="bg-sky" />
-      <x-cards.image img="wagi_lauf_francesco_tunzi.jpg" alt="Francesco Tunzi" />
-      <x-cards.image img="wagi_lauf_alessia_ronner.jpg" alt="Alessia Ronner" />
-      <x-cards.image img="wagi_lauf_julien_widmer.jpg" alt="Julien Widmer" />
-      <x-cards.image img="wagi_lauf_jannick_jaindl.jpg" alt="Jannick Jaindl" />
-      <x-cards.image img="wagi_lauf_dario_hasler.jpg" alt="Dario Hasler" />
+      <x-cards.image img="wagi_lauf_1.jpg" alt="Läufer:in" />
+      <x-cards.image img="wagi_lauf_2.jpg" alt="Läufer:in" />
+      <x-cards.image img="wagi_lauf_3.jpg" alt="Läufer:in" />
+      <x-cards.image img="wagi_lauf_4.jpg" alt="Läufer:in" />
+      <x-cards.image img="wagi_lauf_5.jpg" alt="Läufer:in" />
       <x-cards.quote quote="Ich gebe immer vollgas!" author="Dario" class="bg-sky" />
-      <x-cards.image img="wagi_lauf_leontine_stoeckly.jpg" alt="Leontine Stöckly" />
-      <x-cards.image img="wagi_lauf_manuel_bachofen.jpg" alt="Manuel Bachofen" />
+      <x-cards.image img="wagi_lauf_6.jpg" alt="Läufer:in" />
+      <x-cards.image img="wagi_lauf_7.jpg" alt="Läufer:in" />
       <x-cards.quote quote="Ich freue mich auf das Fest und alle Gäste!" author="Elisabeth" class="bg-sky" />
-      <x-cards.image img="wagi_lauf_elisabeth_rickenbach.jpg" alt="Elisabeth Rickenbach" />
+      <x-cards.image img="wagi_lauf_8.jpg" alt="Läufer:in" />
       <x-cards.quote quote="Das Laufen macht mir am meisten Spass." author="Christa" class="bg-sunshine" />
-      <x-cards.image img="wagi_lauf_christa_pantli.jpg" alt="Christa Pantli" />
-      <x-cards.image img="wagi_lauf_nicola_waldmeyer.jpg" alt="Nicola Waldmeyer" />
-      <x-cards.image img="wagi_lauf_urs_dobrew.jpg" alt="Urs Dobrew" />
-      <x-cards.image img="wagi_lauf_sarita_ehrismann.jpg" alt="Sarita Ehrismann" />
-      <x-cards.image img="wagi_lauf_simon_faessler.jpg" alt="Simon Fässler" />
+      <x-cards.image img="wagi_lauf_9.jpg" alt="Läufer:in" />
+      <x-cards.image img="wagi_lauf_10.jpg" alt="Läufer:in" />
+      <x-cards.image img="wagi_lauf_11.jpg" alt="Läufer:in" />
+      <x-cards.image img="wagi_lauf_12.jpg" alt="Läufer:in" />
+      <x-cards.image img="wagi_lauf_13.jpg" alt="Läufer:in" />
       <x-cards.quote quote="Ich renne und bin schnell!" author="Simon" class="bg-sky" />
-      <x-cards.image img="wagi_lauf_marina_hoechner.jpg" alt="Marina Hoechner" />
+      <x-cards.image img="wagi_lauf_14.jpg" alt="Läufer:in" />
       <x-cards.quote quote="Ich freue mich mega auf den Wagilauf!" author="Marina" class="bg-sky" />
-      <x-cards.image img="wagi_lauf_yvonne_brun.jpg" alt="Yvonne Brun" />
-      <x-cards.image img="wagi_lauf_simon_trueeb.jpg" alt="Simon Trueeb" />
-      <x-cards.image img="wagi_lauf_peter_reutlinger.jpg" alt="Peter Reutlinger" />
-      <x-cards.image img="wagi_lauf_lara_schaub.jpg" alt="Lara Schaub" />
+      <x-cards.image img="wagi_lauf_15.jpg" alt="Läufer:in" />
+      <x-cards.image img="wagi_lauf_16.jpg" alt="Läufer:in" />
+      <x-cards.image img="wagi_lauf_17.jpg" alt="Läufer:in" />
+      <x-cards.image img="wagi_lauf_18.jpg" alt="Läufer:in" />
       <x-cards.quote quote="Das wird mega cool." author="Raffaele" class="bg-sky" />
-      <x-cards.image img="wagi_lauf_rafi_milesi.jpg" alt="Raffaele Milesi" />
-      <x-cards.image img="wagi_lauf_andre_kirilowsky.jpg" alt="André Kirilowsk" />
-      <x-cards.image img="wagi_lauf_andreas_jaindl.jpg" alt="Andreas Jaindl" />
-      <x-cards.image img="wagi_lauf_barbara_leonardt.jpg" alt="Barbara Leonardt" />      
-      <x-cards.image img="wagi_lauf_bastian_rechsteiner.jpg" alt="Bastian Rechsteiner" />
-      <x-cards.image img="wagi_lauf_daniel_egger.jpg" alt="Daniel Egger" />
-      <x-cards.image img="wagi_lauf_donatella_di_meo.jpg" alt="Donatella di Meo" />
-      <x-cards.image img="wagi_lauf_doris_glanzmann.jpg" alt="Doris Glanzmann" />
-      <x-cards.image img="wagi_lauf_evangelos_makkas.jpg" alt="Evangelos Makkas" />
-      <x-cards.image img="wagi_lauf_fabio_veiga.jpg" alt="Fabio Veiga" />
-      <x-cards.image img="wagi_lauf_gino_vanini.jpg" alt="Gino Vanini" />
-      <x-cards.image img="wagi_lauf_isa_hofmann.jpg" alt="Isa Hofmann" />
-      <x-cards.image img="wagi_lauf_isabelle_meier.jpg" alt="Isabelle Meier" />
-      <x-cards.image img="wagi_lauf_jasmin_ulrich.jpg" alt="Jasmin Ulrich" />
-      <x-cards.image img="wagi_lauf_juergen_faes.jpg" alt="Jürgen Faes" />
-      <x-cards.image img="wagi_lauf_karin_meier.jpg" alt="Karin Meier" />
-      <x-cards.image img="wagi_lauf_luc_gallati.jpg" alt="Luc Gallati" />
-      <x-cards.image img="wagi_lauf_marcel_strehler.jpg" alt="Marcel Strehler" />
-      <x-cards.image img="wagi_lauf_marina_scanzi.jpg" alt="Marina Scanzi" />
-      <x-cards.image img="wagi_lauf_nicolas_holenstein.jpg" alt="Nicolas Holenstein" />
-      <x-cards.image img="wagi_lauf_roger_ganz.jpg" alt="Roger Ganz" />
-      <x-cards.image img="wagi_lauf_seraina_pfisterer.jpg" alt="Seraina Pfisterer" />
-      <x-cards.image img="wagi_lauf_stephanie_bachmann.jpg" alt="Stephanie Bachmann" />
-      <x-cards.image img="wagi_lauf_susi_brinkmann.jpg" alt="Susi Brinkmann" />
-      <x-cards.image img="wagi_lauf_tamara_mitrovic.jpg" alt="Tamara Mitrovic" />
+      <x-cards.image img="wagi_lauf_19.jpg" alt="Läufer:in" />
+      <x-cards.image img="wagi_lauf_20.jpg" alt="Läufer:in" />
+      <x-cards.image img="wagi_lauf_21.jpg" alt="Läufer:in" />
+      <x-cards.image img="wagi_lauf_22.jpg" alt="Läufer:in" />      
+      <x-cards.image img="wagi_lauf_23.jpg" alt="Läufer:in" />
+      <x-cards.image img="wagi_lauf_24.jpg" alt="Läufer:in" />
+      <x-cards.image img="wagi_lauf_25.jpg" alt="Läufer:in" />
+      <x-cards.image img="wagi_lauf_26.jpg" alt="Läufer:in" />
+      <x-cards.image img="wagi_lauf_27.jpg" alt="Läufer:in" />
+      <x-cards.image img="wagi_lauf_28.jpg" alt="Läufer:in" />
+      <x-cards.image img="wagi_lauf_29.jpg" alt="Läufer:in" />
+      <x-cards.image img="wagi_lauf_30.jpg" alt="Läufer:in" />
+      <x-cards.image img="wagi_lauf_31.jpg" alt="Läufer:in" />
+      <x-cards.image img="wagi_lauf_32.jpg" alt="Läufer:in" />
+      <x-cards.image img="wagi_lauf_33.jpg" alt="Läufer:in" />
+      <x-cards.image img="wagi_lauf_34.jpg" alt="Läufer:in" />
+      <x-cards.image img="wagi_lauf_35.jpg" alt="Läufer:in" />
+      <x-cards.image img="wagi_lauf_36.jpg" alt="Läufer:in" />
+      <x-cards.image img="wagi_lauf_37.jpg" alt="Läufer:in" />
+      <x-cards.image img="wagi_lauf_38.jpg" alt="Läufer:in" />
+      <x-cards.image img="wagi_lauf_39.jpg" alt="Läufer:in" />
+      <x-cards.image img="wagi_lauf_40.jpg" alt="Läufer:in" />
+      <x-cards.image img="wagi_lauf_41.jpg" alt="Läufer:in" />
+      <x-cards.image img="wagi_lauf_42.jpg" alt="Läufer:in" />
+      <x-cards.image img="wagi_lauf_43.jpg" alt="Läufer:in" />
     </div>
   </x-layout.inner>
 </x-layout.section>
